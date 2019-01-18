9f380821bfeee6610836b41f830c01479f191202
Amund Tenstad <github@amund.io>
2019-01-18 22:31:39 +0100

    Simplify code

--- functions.py
+++ functions.py
@@ -2,21 +2,27 @@ import pygame
 import os
 from PIL import Image, ImageSequence
 
-def asset_load(file, path):
-    if file.endswith(".png"):
-        return [pygame.image.load(os.path.join(path, file))]
-    if file.endswith(".wav"):
-        return pygame.mixer.Sound(os.path.join(path, file))
-    if file.endswith(".gif"):
-        return [pygame.image.fromstring(i.convert("RGBA").tobytes(), i.size, "RGBA") for i in ImageSequence.Iterator(Image.open(os.path.join(path, file)))]
 
-def get_assets(path_assets):
-    asset_list = [asset_load(os.path.join(path_assets, path), path_assets) for path in sorted(os.listdir(path_assets))]
-    name_list = [os.path.basename(path) for path in sorted(os.listdir(path_assets))]
-    dictionary = {}
-    for i in range(len(asset_list)):
-        dictionary[name_list[i]] = asset_list[i]
-    return dictionary
+def load_gif(path):
+    frames = ImageSequence.Iterator(Image.open(path))
+    return [pygame.image.fromstring(frame.convert("RGBA").tobytes(), frame.size, "RGBA") for frame in frames]
+
+
+def load_asset(path):
+    ext = os.path.splitext(path)[1]
+    return {
+        '.png': lambda path: [pygame.image.load(path)],
+        '.wav': pygame.mixer.Sound,
+        '.gif': load_gif,
+    }[ext](path)
+
+
+def get_assets(assets_path):
+    return dict(
+        (os.path.basename(filename), load_asset(os.path.join(assets_path, filename)))
+        for filename in sorted(os.listdir(assets_path))
+    )
+
 
 def instance_place(instance, x, y, instance_list):
     rect = instance.rect.move(x - instance.rect.x, y - instance.rect.y)
@@ -24,6 +30,7 @@ def instance_place(instance, x, y, instance_list):
         if rect.colliderect(i.rect):
             return i
     return False
+
 
 def instance_find(list, var, val):
     return [i for i in list if hasattr(i, var) and getattr(i, var) == val]
