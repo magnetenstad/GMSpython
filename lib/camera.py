9f6be20b7f2dddbb249b84a2e5a0bdb9f79237ba
Amund Tenstad <github@amund.io>
2019-01-29 11:18:51 +0100

    Make camera follow instance

--- lib/camera.py
+++ lib/camera.py
@@ -7,31 +7,16 @@ class Camera(Object):
         super().__init__(x, y)
         self.x = 0
         self.y = 0
-        self.target_x = self.x
-        self.target_y = self.y
         self.width = display_size[0]
         self.height = display_size[1]
-        self.follow_tag = None
         self.follow_instance = None
-        self.follow_search_interval = 30
-        self.follow_search_counter = 0
 
-    def follow(self, tag):
-        self.follow_tag = tag
-        self.search_for_target()
-
-    def search_for_target(self):
-        self.follow_search_counter = 0
-        instances = instance_find_tag(self.follow_tag)
-        self.follow_instance = instances[0] if instances else None
+    def follow(self, instance):
+        self.follow_instance = instance
 
     def step(self):
-        self.follow_search_counter += 1
-        if self.follow_search_counter >= self.follow_search_interval:
-            self.search_for_target()
-
         if not self.follow_instance is None:
-            self.target_x = self.follow_instance.rect.x - self.width / 2
-            self.target_y = self.follow_instance.rect.y - self.height / 2
-            self.x += (self.target_x - self.x) / 10
-            self.y += (self.target_y - self.y) / 10
+            target_x = self.follow_instance.rect.x - self.width / 2
+            target_y = self.follow_instance.rect.y - self.height / 2
+            self.x += (target_x - self.x) / 10
+            self.y += (target_y - self.y) / 10
--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from lib.classes.wall import Wall
 from lib.game import game
 from player import Player
 
+
 def main():
     game.init()
-    game.camera.follow('player')
 
-    Player(100, 100).add_tag('player')
+    player = Player(100, 100)
+    game.camera.follow(player)
 
     for i in range(200):
         Wall(48 + i * 16, 132)
 
     game.run()
+
 
 if __name__ == '__main__':
     main()
